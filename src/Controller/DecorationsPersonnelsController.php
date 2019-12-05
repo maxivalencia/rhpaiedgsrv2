ad9799ac132ab6b5cc883a288b8bf98b8f9f7e7c
kaira <husaynmozart@gmail.com>
2019-12-05 14:48:05 +0300

    index finalisationé

--- src/Controller/DecorationsPersonnelsController.php
+++ src/Controller/DecorationsPersonnelsController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/decorations/personnels")
+ * @Route("/decorationspersonnels")
  */
 class DecorationsPersonnelsController extends AbstractController
 {
--- src/Controller/PersonnelsController.php
+++ src/Controller/PersonnelsController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/personnels")
+ * @Route("/personnelsactifs")
  */
 class PersonnelsController extends AbstractController
 {
